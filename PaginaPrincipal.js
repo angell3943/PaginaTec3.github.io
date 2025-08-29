b4d2f09e872e18da767996112c482cd0a18cfc1d
Angel <Joseangelcoronel0425@gmail.com>
2025-08-29 15:29:26 -0300

    Incorporacion del slider, nuevas fotos en la seccion de presentacion y una actualizacion de la misma

--- PaginaPrincipal.js
+++ PaginaPrincipal.js
@@ -1,22 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const boton = document.getElementById('MenuHamburguesa');
-  const links = document.getElementById('LinksMenu');
+const boton = document.getElementById('MenuHamburguesa');
+const links = document.getElementById('LinksMenu');
 
-    boton.addEventListener('click', () => {
-      links.classList.toggle('activo');
-      });
-const presentacion = document.getElementById('Presentacion');
-const imagenes = [
-  "hero.jpg",
-  "hero.jpg",
-  "hero2.jpg"
-];
-let indice = 0;
-
-setInterval(() => {
-  indice = (indice + 1) % imagenes.length;
-  presentacion.style.backgroundImage = `url('${imagenes[indice]}')`;
-}, 3000);
+  boton.addEventListener('click', () => {
+    links.classList.toggle('activo');
+    });
 
 });
