f98dbfa16f47c0b0e1cb805984812ecea3b099b9
Tomas Valentin Bertuzzo <tomybertu@hotmail.com>
2025-09-07 19:36:05 -0300

    Tarjetas página egresados

---
+++ Scripts/Egresados.js
@@ -0,0 +1,13 @@
+const btnArriba = document.querySelector("#tgl-btn-arriba");
+const btnAbajo = document.querySelector("#tgl-btn-abajo");
+const tarjetas = document.querySelector(".tarjetas-container");
+
+function toggleTarjetas() {
+  tarjetas.classList.toggle("show");
+  btnArriba.classList.toggle("active");
+  btnAbajo.classList.toggle("active");
+}
+
+// Asignar evento a ambas flechas
+btnArriba.addEventListener("click", toggleTarjetas);
+btnAbajo.addEventListener("click", toggleTarjetas);
