4930656824ce2dbeb5c0191c49c6ea56203bac0b
Tomas Valentin Bertuzzo <tomybertu@hotmail.com>
2025-09-08 11:13:34 -0300

    Sexto intento de solución tarjetas egresados, se eliminó la carpeta html y se cambiaron toas las rutas de archivos respectivas

--- Scripts/Egresados.js
+++ Scripts/Egresados.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btnArriba = document.querySelector("#tgl-btn-arriba");
-  const btnAbajo = document.querySelector("#tgl-btn-abajo");
-  const tarjetas = document.querySelector(".tarjetas-container");
+const btnArriba = document.querySelector("#tgl-btn-arriba");
+const btnAbajo = document.querySelector("#tgl-btn-abajo");
+const tarjetas = document.querySelector(".tarjetas-container");
 
-  function toggleTarjetas() {
-    tarjetas.classList.toggle("show");
-    btnArriba.classList.toggle("active");
-    btnAbajo.classList.toggle("active");
-  }
+function toggleTarjetas() {
+  tarjetas.classList.toggle("show");
+  btnArriba.classList.toggle("active");
+  btnAbajo.classList.toggle("active");
+}
 
-  btnArriba.addEventListener("click", toggleTarjetas);
-  btnAbajo.addEventListener("click", toggleTarjetas);
-});
+btnArriba.addEventListener("click", toggleTarjetas);
+btnAbajo.addEventListener("click", toggleTarjetas);
