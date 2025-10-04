fcfdcb605d4fd508afc9bdc8feffe61880b9ac22
Tomas Valentin Bertuzzo <tomybertu@hotmail.com>
2025-10-04 19:51:01 -0300

    slider 100% funcional inscripciones mobile

--- Scripts/slider-inscripciones.js
+++ Scripts/slider-inscripciones.js
@@ -1,30 +1,24 @@
-const dots = document.querySelectorAll(".dot");
-const slides = document.querySelectorAll(".slider .slide");
-const slider = document.querySelector(".slider");
+//1er slider
 
-let indiceCavernicola = 0;
+const dots = document.querySelectorAll(".dot-st");
+const slider = document.querySelector(".slider-st");
+const slides = document.querySelectorAll(".slider-st .slide-st");
 
-for (let i = 0; i<dots.length; i++){
+let seen_index = 0;
 
-    dots[i].addEventListener("click", function(){
+dots.forEach( (dot, i) => {
 
-        for(let j = 0; j<dots.length; j++){
+    dot.addEventListener("click", function(){
 
-            dots[j].classList.remove("active");
+        dots.forEach(d => {
 
-        }
+            d.classList.remove("active");
+
+        });
 
         this.classList.add("active");
 
-    });
-
-}
-
-for (let i = 0; i<dots.length; i++){
-
-    dots[i].addEventListener("click", function(){
-
-        slides[i].scrollIntoView({ 
+        slides[i].scrollIntoView({
             behavior: "smooth",
             block: "nearest",
             inline: "nearest"
@@ -32,22 +26,106 @@ for (let i = 0; i<dots.length; i++){
 
     });
 
-}
+});
 
-    slider.addEventListener("scroll", function(){
+slider.addEventListener("scroll", function(){
 
-        indiceCavernicola = 1 - indiceCavernicola;
-        dots[indiceCavernicola].classList.add("active");
+    seen_index = Math.round( slider.scrollLeft / slides[0].offsetWidth );
 
-        if(indiceCavernicola == 1){
+    dots.forEach(dot => {
 
-            dots[0].classList.remove("active");
-
-        }else{
-
-            dots[1].classList.remove("active");
-
-        }
-
+        dot.classList.remove("active");
 
     });
+
+    dots[seen_index].classList.add("active");
+
+});
+
+//2do slider
+
+const dots_nd = document.querySelectorAll(".dot-nd");
+const slider_nd = document.querySelector(".slider-nd");
+const slides_nd = document.querySelectorAll(".slider-nd .slide-nd");
+
+let seen_index_nd = 0;
+
+dots_nd.forEach( (dot, i) => {
+
+    dot.addEventListener("click", function(){
+
+        dots_nd.forEach(d => {
+
+            d.classList.remove("active");
+
+        });
+
+        this.classList.add("active");
+
+        slides_nd[i].scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "nearest"
+        });
+
+    });
+
+});
+
+slider_nd.addEventListener("scroll", function(){
+
+    seen_index_nd = Math.round( slider_nd.scrollLeft / slides_nd[0].offsetWidth );
+
+    dots_nd.forEach(dot => {
+
+        dot.classList.remove("active");
+
+    });
+
+    dots_nd[seen_index_nd].classList.add("active");
+
+});
+
+//3er slider
+
+const dots_rd = document.querySelectorAll(".dot-rd");
+const slider_rd = document.querySelector(".slider-rd");
+const slides_rd = document.querySelectorAll(".slider-rd .slide-rd");
+
+let seen_index_rd = 0;
+
+dots_rd.forEach( (dot, i) => {
+
+    dot.addEventListener("click", function(){
+
+        dots_rd.forEach(d => {
+
+            d.classList.remove("active");
+
+        });
+
+        this.classList.add("active");
+
+        slides_rd[i].scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "nearest"
+        });
+
+    });
+
+});
+
+slider_rd.addEventListener("scroll", function(){
+
+    seen_index_rd = Math.round( slider_rd.scrollLeft / slides_rd[0].offsetWidth );
+
+    dots_rd.forEach(dot => {
+
+        dot.classList.remove("active");
+
+    });
+
+    dots_rd[seen_index_rd].classList.add("active");
+
+});
